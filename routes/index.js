bf6571dcab606ccdf9fb9c0ecf3631df08dd311e
restolora <reynan.estolora@volenday.com>
2022-04-01 23:14:36 +0800

    testing deploying

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.send('API is UP');
+	res.send('API is UP!');
 });
 
 module.exports = router;
